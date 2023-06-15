c849c18de261edf40520ca77d1724fa2c138b474
uadarksamurai <itandeducationua@gmail.com>
2023-06-15 16:17:48 +0300

    1.0

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import css from "./Profile.module.css";
+import PropTypes from "prop-types";
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li className={css.labelitem}>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li className={css.labelitem}>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li className={css.labelitem}>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+    avatar: PropTypes.string,
+};
